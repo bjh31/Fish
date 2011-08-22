b96a9dcdb22a21076215aa090db55fb1d0a7b05d
Daniel Loomb <dloomb@gmail.com>
2011-08-23 11:18:47 +1200

    Added ACC

---
+++ Fish/Classes/AccController.h
@@ -0,0 +1,15 @@
+//
+//used to create a global accelerometer recorder
+//
+#import <Foundation/Foundation.h>
+
+
+@interface AccController : NSObject {
+    float gx;
+    float gy;
+}
+
+@property float gx;
+@property float gy;
+
+@end
